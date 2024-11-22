90f53beddb84581fd87fff7f672a773367ae77cf
Marcos Lima <marcos.lima@stormgroup.com.br>
2024-11-22 04:09:32 -0300

    refactor: make walls

--- phys.cpp
+++ phys.cpp
@@ -4,7 +4,7 @@
 
 namespace PHYS
 {
-    b2Vec2 b2Vec2_from_vec2_t(const MODEL::vec2_t& a)
+    b2Vec2 b2Vec2_from_vec2_t(const MODEL::vec2_t &a)
     {
         return b2Vec2(static_cast<float>(a.x),
                       static_cast<float>(a.y));
@@ -20,7 +20,7 @@ namespace PHYS
         return b2CreateWorld(&worldDef);
     }
 
-    vector<b2Vec2> _get_b2vecs_from_ground(const MODEL::CEnv& env)
+    vector<b2Vec2> _get_b2vecs_from_ground(const MODEL::CEnv &env)
     {
         const MODEL::vec_vecs_t GroundPoly = env.get_vecs();
         const int32_t nSize = GroundPoly.size(); // NOLINT(*-narrowing-conversions)
@@ -32,7 +32,7 @@ namespace PHYS
         return vecVertices;
     }
 
-    b2ChainDef _create_chain_def(const vector<b2Vec2>& vecVertices)
+    b2ChainDef _create_chain_def(const vector<b2Vec2> &vecVertices)
     {
         b2ChainDef chainDef = b2DefaultChainDef();
         chainDef.points = vecVertices.data();
@@ -45,9 +45,9 @@ namespace PHYS
         return chainDef;
     }
 
-    void _create_ground_body(CWorld& world, const MODEL::CEnv& env)
+    void _create_ground_body(CWorld &world, const MODEL::CEnv &env)
     {
-        const auto vecVertices= _get_b2vecs_from_ground(env);
+        const auto vecVertices = _get_b2vecs_from_ground(env);
         auto groundDef = b2DefaultBodyDef();
         groundDef.type = b2_staticBody;
         world.m_GroundId = b2CreateBody(world.m_WorldId, &groundDef);
@@ -62,15 +62,15 @@ namespace PHYS
         b2CreateChain(world.m_GroundId, &shapeDef);
     }
 
-    void _make_left_wall(const CWorld &world, const MODEL::CEnv &env) {
-        constexpr float tick = 5.0f;
+    void _make_wall(const b2WorldId worldId, const b2Vec2 pos, const b2Vec2 size)
+    {
         b2BodyDef wallDef = b2DefaultBodyDef();
         wallDef.type = b2_staticBody;
-        const float height = fabs(env._bry - env._tly);
-        const b2Polygon groundBox = b2MakeBox(tick, height);
-        wallDef.position = b2Vec2(env._tlx-tick, (env._bry + env._tly) / 2);
 
-        const b2BodyId wallId = b2CreateBody(world.m_WorldId, &wallDef);
+        const b2Polygon groundBox = b2MakeBox(size.x, size.y);
+        wallDef.position = pos;
+
+        const b2BodyId wallId = b2CreateBody(worldId, &wallDef);
 
         const b2ShapeDef groundShapeDef = b2DefaultShapeDef();
         b2CreatePolygonShape(wallId, &groundShapeDef, &groundBox);
@@ -78,50 +78,39 @@ namespace PHYS
         b2Body_SetUserData(wallId, &ID_GROUND);
     }
 
-    void _make_right_wall(const CWorld &world, const MODEL::CEnv &env) {
-        constexpr float tick = 5.0f;
-        b2BodyDef wallDef = b2DefaultBodyDef();
-        wallDef.type = b2_staticBody;
-        const float height = fabs(env._bry - env._tly);
-        const b2Polygon groundBox = b2MakeBox(tick, height);
-        wallDef.position = b2Vec2(env._brx+tick, (env._bry + env._tly) / 2);
-
-        const b2BodyId wallId = b2CreateBody(world.m_WorldId, &wallDef);
-
-        const b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-        b2CreatePolygonShape(wallId, &groundShapeDef, &groundBox);
-
-        b2Body_SetUserData(wallId, &ID_GROUND);
+    void _make_left_wall(const b2WorldId worldId, const MODEL::CEnv &env, const float tick)
+    {
+        const auto pos = b2Vec2(env._tlx - tick, (env._bry + env._tly) / 2);
+        const auto size = b2Vec2(tick, fabs(env._bry - env._tly));
+        _make_wall(worldId, pos, size);
     }
 
-    void _make_ceiling(const CWorld &world, const MODEL::CEnv &env) {
-        constexpr float tick = 5.0f;
-        b2BodyDef wallDef = b2DefaultBodyDef();
-        wallDef.type = b2_staticBody;
-        const float width = fabs(env._brx - env._tlx);
-        const b2Polygon groundBox = b2MakeBox(width, tick);
-        wallDef.position = b2Vec2((env._brx + env._tlx) / 2, env._tly+tick);
-
-        const b2BodyId wallId = b2CreateBody(world.m_WorldId, &wallDef);
-
-        const b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-        b2CreatePolygonShape(wallId, &groundShapeDef, &groundBox);
-
-        b2Body_SetUserData(wallId, &ID_GROUND);
+    void _make_right_wall(const b2WorldId worldId, const MODEL::CEnv &env, const float tick)
+    {
+        const auto pos = b2Vec2(env._brx + tick, (env._bry + env._tly) / 2);
+        const auto size = b2Vec2(tick, fabs(env._bry - env._tly));
+        _make_wall(worldId, pos, size);
     }
 
+    void _make_ceiling(const b2WorldId worldId, const MODEL::CEnv &env, const float tick)
+    {
+        const auto pos = b2Vec2((env._brx + env._tlx) / 2, env._tly + tick);
+        const auto size = b2Vec2(fabs(env._brx - env._tlx), tick);
+        _make_wall(worldId, pos, size);
+    }
 
-    void _create_ground(CWorld& world, const MODEL::CEnv& env)
+    void _create_ground(CWorld &world, const MODEL::CEnv &env)
     {
         _create_ground_body(world, env);
 
         // Paredes e teto:
-        _make_left_wall(world, env);
-        _make_right_wall(world, env);
-        _make_ceiling(world, env);
+        constexpr float tick = 5.0f;
+        _make_left_wall(world.m_WorldId, env, tick);
+        _make_right_wall(world.m_WorldId, env, tick);
+        _make_ceiling(world.m_WorldId, env, tick);
     }
 
-    void buildWorld(const MODEL::CEnv& env, CWorld& world)
+    void buildWorld(const MODEL::CEnv &env, CWorld &world)
     {
         world.m_WorldId = _create_world();
         _create_ground(world, env);
